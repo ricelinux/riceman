45b8d31ce1c13f6a9a0ac85124fbcc3720b9b57c
Zaedus <nintendozaedus@gmail.com>
2021-07-05 23:58:46 -0500

    Restructure and handle errors better

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 #include "constants.hpp"
 #include "config.hpp"
 #include "util.hpp"
-
 #include <iostream>
 #include <getopt.h>
 
-using std::cout, std::endl;
+using std::cout, std::endl, std::string;
 
-RicemanConfig* config;
-
-void help(int &op, const char * const pname)
+void help(int &op, string pname)
 {
     cout << pname << endl;
 }
@@ -21,8 +18,6 @@ void version()
          << endl
          << "This program may be freely redistributed under" << endl
          << "the terms of the GNU General Public License." << endl;
-
-
 }
 
 static int parsearg_op(int opt, bool dryrun)
@@ -30,13 +25,13 @@ static int parsearg_op(int opt, bool dryrun)
     switch(opt) {
         case 'S':
             if(dryrun) break;
-            config->op = (config->op != OP_MAIN ? 0 : OP_SYNC); break;
+            config->op = (config->op != OP_UNSET ? 0 : OP_SYNC); break;
         case 'R':
             if(dryrun) break;
-            config->op = (config->op != OP_MAIN ? 0 : OP_REMOVE); break;
+            config->op = (config->op != OP_UNSET ? 0 : OP_REMOVE); break;
         case 'Q':
             if(dryrun) break;
-            config->op = (config->op != OP_MAIN ? 0 : OP_QUERY); break;
+            config->op = (config->op != OP_UNSET ? 0 : OP_QUERY); break;
         case 'V':
             if(dryrun) break;
             config->version = true; break;
@@ -53,7 +48,7 @@ static int parseargs(int argc, char *argv[])
 {
     int opt;
     int option_index = 0;
-    const char *optstring = "SRQVh";
+    const char *optstring = "SRQVh:";
     static const struct option opts[] =
     {
         { "sync",       no_argument,    0,  'S' },
@@ -61,27 +56,29 @@ static int parseargs(int argc, char *argv[])
         { "query",      no_argument,    0,  'Q' },
         { "version",    no_argument,    0,  'V' },
         { "help",       no_argument,    0,  'h' },
+
         {0, 0, 0, 0}
     };
     
+    // parse operational args
     while((opt = getopt_long(argc, argv, optstring, opts, &option_index)) != -1) {
 		if(opt == 0) {
 			continue;
 		} else if(opt == '?') {
-			/* unknown option, getopt printed an error */
 			return 1;
 		}
 		parsearg_op(opt, 0);
 	}
 
-    if (config->op == OP_MAIN)
+    if (config->op == OP_UNSET)
     {
-        // Handle errors by creating a function in util.cpp
+        log(LOG_ERROR, "too many operations specified");
+        return 1;
     }
 
     if (config->help)
     {
-        help(config->op, mbasename(argv[0]));
+        help(config->op, basename(argv[0]));
         exit(EXIT_SUCCESS);
     }
 
