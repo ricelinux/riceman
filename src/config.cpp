cb4189f0b628708aeef0c27ce14e7b12c738edcf
Zaedus <nintendozaedus@gmail.com>
2021-07-17 00:16:01 -0500

    Implement show_colors function

--- src/config.cpp
+++ src/config.cpp
@@ -5,3 +5,31 @@
 RicemanConfig::RicemanConfig()
     : op{OP_UNSET}, loglevel{LOG_ALL | LOG_ERROR | LOG_WARNING}, color{COLOR_UNSET}
 {}
+
+bool RicemanConfig::show_colors(bool state)
+{
+    if (state) {
+        colors.colon    = BOLDBLUE "::" BOLD " ";
+        colors.title    = BOLD;
+        colors.repo     = BOLDMAGENTA;
+        colors.version  = BOLDGREEN;
+        colors.groups   = BOLDBLUE;
+        colors.meta     = BOLDCYAN;
+        colors.warn     = BOLDYELLOW;
+        colors.err      = BOLDRED;
+        colors.faint    = GREY46;
+        colors.nocolor  = NOCOLOR;
+    } else {
+        colors.colon    = ":: ";
+        colors.title    = "";
+        colors.repo     = "";
+        colors.version  = "";
+        colors.groups   = "";
+        colors.meta     = "";
+        colors.warn     = "";
+        colors.err      = "";
+        colors.faint    = "";
+        colors.nocolor  = "";
+    }
+    
+}
