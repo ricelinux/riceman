308a74b45ca84b4ca429b4984ea8bb712ee44351
Zaedus <nintendozaedus@gmail.com>
2021-07-19 16:15:24 -0500

    rename types to option and add macros

---
+++ src/option.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+#define OPT(shortopt, longopt, defaultval, implicitval, help) { std::string{'-', shortopt}, "--" longopt, defaultval, implicitval, help }
+#define OP_OPT(shortopt, longopt, implicitval, help) OPT(shortopt, longopt, false, implicitval, help)
+
+template <typename T>
+struct option {
+    const std::string shortopt;
+    const std::string longopt;
+    const T defaultval;
+    const T implicitval;
+    const std::string help;
+};
