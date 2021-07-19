e100cabfb6b3d5fcda217dfebbd477f84991ee82
Zaedus <nintendozaedus@gmail.com>
2021-07-19 00:34:25 -0500

    Implement abstract operation class

---
+++ src/operation.cpp
@@ -0,0 +1,4 @@
+#include "operation.hpp"
+
+OperationHandler::OperationHandler(argparse::ArgumentParser &parser, RicemanConfig &conf, Utils util)
+    : argparser{parser}, config{conf}, utils{util} {};
---
+++ src/operation.hpp
@@ -0,0 +1,24 @@
+#ifndef OPERATION_HPP
+#define OPERATION_HPP
+
+#include "config.hpp"
+#include "utils.hpp"
+
+#include <argparse/argparse.hpp>
+
+class OperationHandler
+{
+    public:
+
+    OperationHandler(argparse::ArgumentParser &parser, RicemanConfig &conf, Utils util);
+
+    virtual bool run() = 0;
+
+    protected:
+
+    argparse::ArgumentParser &argparser;
+    RicemanConfig &config;
+    Utils &utils;
+};
+
+#endif
