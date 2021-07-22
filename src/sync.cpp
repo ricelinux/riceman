7411acb7b09c7d0bce3dc0f6aaafc57a66e8f752
Zaedus <nintendozaedus@gmail.com>
2021-07-21 21:38:29 -0500

    Implement basic refresh arg counting

--- src/sync.cpp
+++ src/sync.cpp
@@ -1,10 +1,34 @@
 #include "sync.hpp"
 #include "constants.hpp"
 
-SyncHandler::SyncHandler(argparse::ArgumentParser &parser, RicemanConfig &conf, Utils util)
-    : OperationHandler(parser, conf, util) {};
+/* Init statics */
+const struct option<int> SyncHandler::op_modifiers[] = { 
+    OPT('y', "refresh", 0, 1, "refreshes downloaded databases")
+};
+unsigned short SyncHandler::refresh = 0;
+
+SyncHandler::SyncHandler(argparse::ArgumentParser &argparser, RicemanConfig &config, Utils &utils) 
+: OperationHandler(argparser, config, utils)
+{
+    if (argparser.is_used("--refresh")) {
+        refresh = argparser.get_length("--refresh");
+    }
+}
 
 bool SyncHandler::run()
 {
-    utils.log(LOG_ALL, std::to_string(config.op));
+    switch (refresh)
+    {
+        case 0:
+            utils.log(LOG_ALL, "Database will not be refreshed.");
+            break;
+        case 1:
+            utils.log(LOG_ALL, "Database will be refreshed if there is a new version.");
+            break;
+        default:
+            utils.log(LOG_ALL, "Database will be refreshed no matter what.");
+            break;
+    }
+
+    return true;
 }
--- src/sync.hpp
+++ src/sync.hpp
@@ -1,15 +1,23 @@
 #ifndef SYNC_HPP
 #define SYNC_HPP
 
+#include "option.hpp"
 #include "operation.hpp"
 
 class SyncHandler: public OperationHandler
 {
     public:
 
-    SyncHandler(argparse::ArgumentParser &parser, RicemanConfig &conf, Utils util);
-
+    SyncHandler(argparse::ArgumentParser &parser, RicemanConfig &conf, Utils &util);
     bool run();
+
+    static const int s_op_modifiers = 1;
+    static const struct option<int> op_modifiers[s_op_modifiers];
+
+    private:
+
+    /* Public Operation Modifiers */
+    static unsigned short refresh;
 };
 
 #endif
