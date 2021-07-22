4957b4166b50151815c8a8cbc2abfdc38e7684df
Zaedus <nintendozaedus@gmail.com>
2021-07-21 21:35:01 -0500

    Add argument macros

--- src/option.hpp
+++ src/option.hpp
@@ -6,6 +6,22 @@
 #define OPT(shortopt, longopt, defaultval, implicitval, help) { std::string{'-', shortopt}, "--" longopt, defaultval, implicitval, help }
 #define OP_OPT(shortopt, longopt, implicitval, help) OPT(shortopt, longopt, false, implicitval, help)
 
+#define ADD_ARGUMENTS(argparser, opts) \
+for (int i = 0; i < STRUCT_LEN(opts); i++) { \
+    argparser.add_argument(opts[i].shortopt, opts[i].longopt) \
+        .implicit_value(opts[i].implicitval) \
+        .default_value(opts[i].defaultval) \
+        .help(opts[i].help); \
+}
+#define ADD_COMPOUND_ARGUMENTS(argparser, opts) \
+for (int i = 0; i < STRUCT_LEN(opts); i++) { \
+    argparser.add_argument(opts[i].shortopt, opts[i].longopt) \
+        .nargs(0) \
+        .append() \
+        .implicit_value(opts[i].implicitval) \
+        .help(opts[i].help); \
+}
+
 template <typename T>
 struct option {
     const std::string shortopt;
