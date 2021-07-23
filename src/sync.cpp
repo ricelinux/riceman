1292eb559947dfb65885d813ba4122c6496b9ec3
Zaedus <nintendozaedus@gmail.com>
2021-07-23 16:30:28 -0500

    Add upgrade option

--- src/sync.cpp
+++ src/sync.cpp
@@ -2,21 +2,19 @@
 #include "constants.hpp"
 
 /* Init statics */
-const struct option<int> SyncHandler::op_modifiers[] = {
-    OPT('u', "upgrade", 0, 1, "upgrades installed rices"),
-};
-
-const struct option<int> SyncHandler::compound_op_modifiers[] = { 
+const struct option<int> SyncHandler::op_modifiers[] = { 
     OPT('y', "refresh", 0, 1, "refreshes downloaded databases"),
+    OPT('u', "upgrade", 0, 1, "upgrades installed rices")
 };
-unsigned short SyncHandler::refresh = 0;
 
-/* Main Code */
 SyncHandler::SyncHandler(argparse::ArgumentParser &argparser, RicemanConfig &config, Utils &utils) 
 : OperationHandler(argparser, config, utils)
 {
     if (argparser.is_used("--refresh")) {
         refresh = argparser.get_length("--refresh");
+    }
+    if (argparser.is_used("--upgrade")) {
+        upgrade = argparser.get<int>("--upgrade");
     }
 }
 
@@ -33,6 +31,10 @@ bool SyncHandler::run()
         default:
             utils.log(LOG_ALL, "Database will be refreshed no matter what.");
             break;
+    }
+
+    if (upgrade) {
+        utils.log(LOG_ALL, "Upgrading all rices.");
     }
 
     return true;
