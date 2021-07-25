8d4b49e8c5ce4ec449d5c70c86d90a2b27973a0d
Zaedus <nintendozaedus@gmail.com>
2021-07-24 19:50:36 -0500

    Begin creating database functionality

--- src/database.cpp
+++ src/database.cpp
@@ -1,7 +1,30 @@
 #include "database.hpp"
+#include "constants.hpp"
 
-Database::Database(std::string local_path, std::string remote_uri)
-: local_path{local_path}, remote_uri{remote_uri}
+Database::Database(std::string localpath, std::string remoteuri)
+: local_path{localpath}, remote_uri{remoteuri}
+{}
+
+size_t Database::write_functor(cURLpp::Easy *handle, std::fstream *stream, char *ptr, size_t size, size_t nmemb)
 {
+    size_t realsize = size * nmemb;
 
+    stream->write(ptr, realsize);
+
+    return realsize;
+}
+
+const std::string Database::get_remote_hash()
+{
+    curlpp::Easy request;
+
+    std::fstream stream{local_path, std::fstream::out};
+
+    using namespace std::placeholders;
+    curlpp::options::WriteFunction *write_func = new curlpp::options::WriteFunction(std::bind(&Database::write_functor, this, &request, &stream, _1, _2, _3));
+
+    request.setOpt(write_func);
+    request.setOpt(new curlpp::options::Url(REMOTE_RICE_DB));
+    
+    request.perform();
 }
--- src/database.hpp
+++ src/database.hpp
@@ -2,17 +2,27 @@
 #define DATABASE_HPP
 
 #include <string>
+#include <fstream>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Exception.hpp>
+#include <curlpp/Options.hpp>
 
 class Database
 {
     public:
-    
-    Database(std::string local_path, std::string remote_uri);
+    Database(std::string localpath, std::string remoteuri);
 
-    private:
+    const std::string get_remote_hash();
+    const std::string get_local_hash();
 
     const std::string local_path;
     const std::string remote_uri;
+
+    private:
+
+    size_t write_functor(cURLpp::Easy *handle, std::fstream *stream, char *ptr, size_t size, size_t nmemb);
+    
 };
 
 #endif // DATABASE_HPP
