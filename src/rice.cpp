664c5207d88a99975c093a8a476f88f1530c0dae
Zaedus <nintendozaedus@gmail.com>
2021-07-31 03:32:47 -0500

    Parse toml, clone git repo, begin working with CBs

--- src/rice.cpp
+++ src/rice.cpp
@@ -5,26 +5,83 @@
 
 #include <fmt/format.h>
 #include <cpr/cpr.h>
-#include "git2.h"
+#include <git2.h>
+#include <cpptoml.h>
 
 Rice::Rice(std::string name, std::string id, std::string description, std::string version, std::string window_manager, std::vector<Dependency> dependencies, bool installed)
-: name{name}, id{id}, description{description}, version{version}, window_manager{window_manager}, dependencies{dependencies}, installed{installed}, file_path{fmt::format("{}/{}.toml", LOCAL_TOML_DIR, id)}
+: name{name}, id{id}, description{description}, version{version}, window_manager{window_manager}, dependencies{dependencies}, installed{installed}, toml_path{fmt::format("{}/{}.toml", LOCAL_CONFIG_DIR, id)}, git_path{fmt::format("{}/{}", LOCAL_RICES_DIR, id)}
 {}
+
+static int sideband_progress(const char *str, int len, void *payload)
+{
+	(void)payload; /* unused */
+
+	printf("remote: %.*s", len, str);
+	fflush(stdout);
+	return 0;
+}
+
+static int fetch_progress(const git_indexer_progress *stats, void *payload)
+{
+	progress_data *pd = (progress_data*)payload;
+	pd->fetch_progress = *stats;
+	std::cout << pd->completed_steps << "/" << pd->total_steps << std::endl;
+	return 0;
+}
+static void checkout_progress(const char *path, size_t cur, size_t tot, void *payload)
+{
+	progress_data *pd = (progress_data*)payload;
+	pd->completed_steps = cur;
+	pd->total_steps = tot;
+	pd->path = path;
+	std::cout << pd->completed_steps << "/" << pd->total_steps << std::endl;
+}
+
+void Rice::cred_acquire(git_credential **out, const char *url, const char *username_from_url, unsigned int allowed_types, void *payload) {
+    return throw std::runtime_error{fmt::format("'{}' requires additional authentication", url)};
+}
 
 void Rice::install()
 {
+    git_libgit2_init();
+
     /* Download rice's toml */
     using namespace std::placeholders;
-
-    ProgressBar progress_bar{fmt::format("{}-{}", name, version), 0.4};
+    ProgressBar clone_progress_bar{"cloning rice repo", 0.4};
+    ProgressBar progress_bar{fmt::format("downloading rice config", name, version), 0.4};
     cpr::Response r = cpr::Get(cpr::Url{fmt::format("{}/{}.toml", REMOTE_RICES_URI, id)}, cpr::ProgressCallback{std::bind(&ProgressBar::progress_callback_download, &progress_bar, _1, _2, _3, _4)});
     progress_bar.done();
 
-    if (!Utils::write_file_content(file_path, r.text)) throw std::runtime_error{fmt::format("unable to write to '{}'", file_path)};
+    if (!Utils::write_file_content(toml_path, r.text)) throw std::runtime_error{fmt::format("unable to write to '{}'", toml_path)};
 
-}
-
-void Rice::clone_progress(const git_transfer_progress *stats, void *payload)
-{
+    /* Parse TOML */
+    auto rice_config = cpptoml::parse_file(toml_path);
     
+    git_repo_uri = rice_config->get_qualified_as<std::string>("git.repo").value_or("");
+    git_commit_hash = rice_config->get_qualified_as<std::string>("git.commit").value_or("");
+
+    if (git_repo_uri.length() == 0) throw std::runtime_error{fmt::format("git repository uri not specified in '{}' config", name)};
+    if (git_commit_hash.length() == 0) throw std::runtime_error{fmt::format("git commit hash not specified in '{}' config", name)};
+
+    /* Clone git repo */
+    progress_data pd = {{0}};
+    git_repository *repo = nullptr;
+    git_clone_options clone_opts = GIT_CLONE_OPTIONS_INIT;
+    git_checkout_options checkout_opts = GIT_CHECKOUT_OPTIONS_INIT;
+    
+    checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
+	checkout_opts.progress_cb = checkout_progress;
+	checkout_opts.progress_payload = &pd;
+	clone_opts.checkout_opts = checkout_opts;
+	clone_opts.fetch_opts.callbacks.sideband_progress = sideband_progress;
+	clone_opts.fetch_opts.callbacks.transfer_progress = &fetch_progress;
+	clone_opts.fetch_opts.callbacks.credentials = (git_credential_acquire_cb)Rice::cred_acquire;
+	clone_opts.fetch_opts.callbacks.payload = &pd;
+
+    int clone_error = git_clone(&repo, git_repo_uri.c_str(), git_path.c_str(), &clone_opts);
+    if (clone_error != 0) {
+        const git_error *err = git_error_last();
+		if (err) throw std::runtime_error{fmt::format("{} (error code {})", err->message, err->klass)};
+		else throw std::runtime_error{fmt::format("error cloning git repository", clone_error)};
+    }
 }
--- src/rice.hpp
+++ src/rice.hpp
@@ -8,6 +8,13 @@
 
 namespace fs = std::filesystem;
 
+typedef struct progress_data {
+	git_indexer_progress fetch_progress;
+	size_t completed_steps;
+	size_t total_steps;
+	const char *path;
+} progress_data;
+
 typedef struct Dependency {
     bool aur;
     std::string name;
@@ -17,7 +24,13 @@ class Rice
 {
     public:
 
-    Rice(std::string name, std::string id, std::string description, std::string version, std::string window_manager, std::vector<Dependency> dependencies, bool installed);
+    Rice(std::string name, 
+        std::string id, 
+        std::string description, 
+        std::string version, 
+        std::string window_manager,
+        std::vector<Dependency> dependencies, 
+        bool installed);
 
     void install();
 
@@ -27,11 +40,20 @@ class Rice
     const std::string description;
     const std::string version;
     const std::string window_manager;
-    const std::string file_path;
+    const std::string toml_path;
+    const std::string git_path;
     std::vector<Dependency> dependencies; 
+
+    std::string git_repo_uri;
+    std::string git_commit_hash;
 
     private:
     
-    void clone_progress(const git_transfer_progress *stats, void *payload);
+    static void clone_progress(const progress_data *pd);
+    static void cred_acquire(git_credential **out,
+		const char *url,
+		const char *username_from_url,
+		unsigned int allowed_types,
+		void *payload);
 
 };
