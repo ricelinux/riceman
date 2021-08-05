4c141edaa6090bf6266bd3bcea3ec1531a59d506
Zaedus <nintendozaedus@gmail.com>
2021-08-05 00:30:30 -0500

    Update constants

--- src/constants.hpp
+++ src/constants.hpp
@@ -11,8 +11,11 @@
 #define VERSION "v1.0.0"
 #endif
 
-/* AUR Helper */
+/* Dependency Management */
 #define AUR_HELPER "yay"
+#define PACMAN_FLAGS "--noconfirm"
+#define AUR_HELPER_FLAGS "--nouseask", "--nopgpfetch", "--noupgrademenu", "--nodiffmenu", "--noeditmenu", "--nocleanmenu", PACMAN_FLAGS
+
 
 /* Repository Info */
 #define DB_NAME "rices.db"
