de4010f0aa1c2fd95e1f2f28edfafa80c8ac8078
Zaedus <nintendozaedus@gmail.com>
2021-08-06 11:27:05 -0500

    Implement exec_pacman

--- src/package_manager.cpp
+++ src/package_manager.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <iostream>
 
+#include <unistd.h>
+#include <sys/wait.h>
+
 void PackageManager::install_diff(DependencyVec &old_deps, DependencyVec &new_deps)
 {
     DependencyVec remove_deps;
@@ -33,3 +36,45 @@ void PackageManager::install_diff(DependencyVec &old_deps, DependencyVec &new_de
     for (Dependency &dep : add_deps) install(dep);
 }
 
+void PackageManager::install(Dependency &dep)
+{
+    if (dep.aur) install_aur(dep);
+    else install_pacman(dep);
+}
+
+void PackageManager::install_pacman(Dependency &dep)
+{
+
+}
+
+void PackageManager::install_aur(Dependency &dep)
+{
+
+}
+
+void PackageManager::remove(Dependency &dep)
+{
+
+}
+
+void PackageManager::exec_pacman(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            throw std::runtime_error{"failed to fork child process"};
+        }
+    } else if (pid > 0) {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    } else {
+        throw std::runtime_error{"failed to fork child process"};
+    }
+
+
+}
