5e9b1af31d1cf8837afa3712a0097b741e8f7bdf
Zaedus <nintendozaedus@gmail.com>
2021-08-06 01:05:26 -0500

    Finish install_diff

--- src/package_manager.cpp
+++ src/package_manager.cpp
@@ -5,17 +5,30 @@
 
 void PackageManager::install_diff(DependencyVec &old_deps, DependencyVec &new_deps)
 {
-    DependencyVec intersection;
+    DependencyVec remove_deps;
+    DependencyVec add_deps;
 
     std::sort(old_deps.begin(), old_deps.end());
     std::sort(new_deps.begin(), new_deps.end());
 
-    std::set_intersection(
+    std::set_difference(
         old_deps.begin(), old_deps.end(),
         new_deps.begin(), new_deps.end(),
-        back_inserter(intersection),
+        back_inserter(remove_deps),
         [](const Dependency &a, const Dependency &b) {
             return (a.name < b.name) || a.aur != b.aur;
         }
     );
+
+    std::set_difference(
+        new_deps.begin(), new_deps.end(),
+        old_deps.begin(), old_deps.end(),
+        back_inserter(add_deps),
+        [](const Dependency &a, const Dependency &b) {
+            return (a.name < b.name) || a.aur != b.aur;
+        }
+    );
+    
+    for (Dependency &dep : remove_deps) remove(dep);
+    for (Dependency &dep : add_deps) install(dep);
 }
