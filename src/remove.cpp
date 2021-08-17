ccbfd77c3df38709f17d9a269a5f24cd77f49677
Zaedus <nintendozaedus@gmail.com>
2021-08-16 21:37:14 -0500

    Setup RemoveHandler class, handle remove opts

--- src/remove.cpp
+++ src/remove.cpp
@@ -1,1 +1,15 @@
 #include "remove.hpp"
+
+const struct option<int> RemoveHandler::op_modifiers[RemoveHandler::op_modifiers_s] = {
+    OPT('g', "remove-git",      0,  1,  "removes the git repository"),
+    OPT('t', "remove-toml",     0,  1,  "removes the rice config"),
+    OPT('d', "keep-desktop",    0,  1,  "ignores the removal of the desktop entry")
+};
+
+RemoveHandler::RemoveHandler(argparse::ArgumentParser &parser, RicemanConfig &conf, Utils &util, DatabaseCollection &database_col)
+: OperationHandler(parser, conf, util, database_col)
+{
+    remove_git   = argparser.is_used("--remove-git");
+    remove_toml  = argparser.is_used("--remove-toml");
+    keep_desktop = argparser.is_used("--keep-desktop");
+}
--- src/remove.hpp
+++ src/remove.hpp
@@ -1,1 +1,23 @@
-#pragma 
+#pragma once
+
+#include "option.hpp"
+#include "operation.hpp"
+#include "constants.hpp"
+
+class RemoveHandler: public OperationHandler
+{
+    public:
+
+    RemoveHandler(argparse::ArgumentParser &parser, RicemanConfig &conf, Utils &util, DatabaseCollection &databases);
+    bool run();
+
+    static const int op_modifiers_s = 2;
+    static const struct option<int> op_modifiers[op_modifiers_s];
+
+    std::vector<std::string> targets;
+
+    /* Operation Modifiers */
+    bool remove_git;
+    bool remove_toml;
+    bool keep_desktop;
+};
