144fe476c939f8c95acf2d5ef028fc4529ee0931
Zaedus <nintendozaedus@gmail.com>
2021-08-23 18:21:29 -0500

    Implement confirmation dialog and other logs

--- src/remove.cpp
+++ src/remove.cpp
@@ -16,7 +16,7 @@ const struct option<int> RemoveHandler::op_modifiers[RemoveHandler::op_modifiers
 RemoveHandler::RemoveHandler(argparse::ArgumentParser &parser, RicemanConfig &conf, Utils &util, DatabaseCollection &database_col)
 : OperationHandler(parser, conf, util, database_col)
 {
-    if (argparser.is_used("complete")) {
+    if (argparser.is_used("--complete")) {
         remove_git = true;
         remove_toml = true;
         remove_desktop = true;
@@ -38,7 +38,9 @@ void RemoveHandler::run()
         /* Verify to-be-installed rices */
         for(std::string &target : targets) {
             try {
-                rices.push_back(databases.get_rice(target));
+                Rice rice = databases.get_rice(target);
+                std::cout << rice.install_state << std::endl;
+                if (rice.install_state != Rice::NOT_INSTALLED) rices.push_back(rice);
             } catch (std::runtime_error err) {
                 incorrect_rice_names.push_back(target);
             }
@@ -50,29 +52,66 @@ void RemoveHandler::run()
 
 void RemoveHandler::remove_rices()
 {
+    std::string dependency_string;
     utils.colon_log("Removing rices...");
     utils.rice_log(rices);
 
     for (Rice &rice : rices) {
+        for (Dependency &dep : rice.old_dependencies) {
+            dependency_string.append(dep.name).append(" ");
+        }
+    }
+
+    std::cout << config.colors.title << "Removed Rices:\t"
+            << config.colors.nocolor << (dependency_string.length() == 0 ? "None" : dependency_string) 
+            << std::endl << std::endl;
+    
+    utils.colon_log("Proceed with removal? [Y/n] ", false);
+
+    const char confirm = std::getchar();
+    if (confirm != '\n' && confirm != 'y' && confirm != 'Y' && confirm != ' ') utils.log(LOG_FATAL, "removal aborted");
+
+    Utils::show_cursor(false);
+    Utils::handle_signals(true);
+
+    utils.colon_log("Processing rice changes...");
+
+    for (Rice &rice : rices) {
+        ProgressBar pb{" " + rice.name, 0.4};
+
         try {
-            if ((rice.install_state | Rice::DESKTOP_INSTALLED) && remove_desktop) 
+            if ((rice.install_state & Rice::DESKTOP_INSTALLED) != 0 && remove_desktop) 
                 fs::remove(rice.desktop_path);
+            pb.update("", 0.33);
         } catch (fs::filesystem_error) {
             utils.log(LOG_ERROR, fmt::format("failed to remove desktop entry for '{}'", rice.name));
         }
 
         try {
-            if ((rice.install_state | Rice::GIT_INSTALLED) && remove_git) 
+            if ((rice.install_state & Rice::GIT_INSTALLED) != 0 && remove_git) 
                 fs::remove(rice.git_path);
+            pb.update("", 0.66);
         } catch (fs::filesystem_error) {
             utils.log(LOG_ERROR, fmt::format("failed to remove git repository for '{}'", rice.name));
         }
 
         try {
-            if ((rice.install_state | Rice::TOML_INSTALLED) && remove_toml) 
+            if ((rice.install_state & Rice::TOML_INSTALLED) != 0 && remove_toml) 
                 fs::remove(rice.toml_path);
+            pb.update("", 1);
         } catch (fs::filesystem_error) {
             utils.log(LOG_ERROR, fmt::format("failed to remove rice config for '{}'", rice.name));
         }
+
+        pb.done();
     }
+
+    utils.colon_log("Processing package changes...");
+
+    for (Rice &rice : rices) {
+        PackageManager::remove(rice.old_dependencies, utils.remove_confirmation_dialog(rice.old_dependencies));
+    }
+
+    Utils::show_cursor(true);
+    Utils::handle_signals(false);
 }
